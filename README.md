8570bac02be5c82bb773935669bca5df6e7bb6df
sourabhvishnav <62550282+sourabhvishnav@users.noreply.github.com>
2020-08-14 07:27:16 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# BatmanBegins
-Batman.....
+# PROC41-Template
---
+++ sketch.js
@@ -0,0 +1,78 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var man_image;
+var ground;
+var ground_options;
+var maxDrops = 100;
+var drops = [];
+var thunder1, thunder2, thunder3;
+var umbrella;
+
+function preload(){
+  man_image = loadImage("Man.png");
+  thunder1 = loadImage("thunder1.png");
+  thunder2 = loadImage("thunder2.png");
+  thunder3 = loadImage("thunder3.png");
+}
+
+function setup(){
+  var canvas = createCanvas(500,500);
+
+  engine = Engine.create();
+  world  = engine.world;
+
+  
+  umbrella = new Umbrella();
+
+  if(frameCount%100===0){
+    for(var i=0; i<maxDrops;i++){
+      drops.push(new Drop(random(0,400),random(0,400),3,10));
+     }
+    }
+
+}
+
+function draw(){
+  background(0);
+  Engine.update(engine);
+
+  var rand = Math.round(random(1,2));
+  if(frameCount%80===0){
+   var thunderCreatedFrame = frameCount;
+   var thunder = createSprite(random(10,370),random(10,30),10,10);
+   switch(rand){
+     case 1 : thunder.addImage(thunder1);
+     break;
+     case 2 : thunder.addImage(thunder2);
+     break;
+     case 3 : thunder.addImage(thunder3);
+     break;
+     default : break;
+   }
+   console.log(thunderCreatedFrame);
+
+   if(frameCount %30===0){
+     thunder.destroy();
+   }
+
+  //  if(thunderCreatedFrame + 20===frameCount && thunder){
+  //     thunder.destroy();
+  //  }
+  }
+
+  umbrella.display();
+  
+  for(var i=0; i<maxDrops;i++){
+    drops[i].display();
+    drops[i].update();
+  }
+ 
+  
+  drawSprites();
+}
+
+
